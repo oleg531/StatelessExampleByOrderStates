2884ea9a580266d0fac00fb36e8f65fce8038ba2
Oleg <Oleg531@gmail.com>
2017-01-30 18:26:45 +0300

    Add StateMachineFactory - base implementation

---
+++ src/StatelessExampleByOrderStates/Models/Stateless/OrderTriggers.cs
@@ -0,0 +1,9 @@
+﻿namespace StatelessExampleByOrderStates.Models.Stateless
+{
+    public enum OrderTriggers
+    {
+        ApproveByLocalUser = 10,
+        ApproveByMainUser = 20,
+        Reject = 30
+    }
+}
---
+++ src/StatelessExampleByOrderStates/Models/Stateless/StateMachineFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace StatelessExampleByOrderStates.Models.Stateless
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using global::Stateless;
+
+    public static class StateMachineFactory
+    {
+        public static StateMachine<OrderStates, OrderTriggers> Create(Order order)
+        {
+            var stateMachine = new StateMachine<OrderStates, OrderTriggers>(() => order.OrderState, s => order.OrderState = s);
+            return Configure(stateMachine);
+        }
+
+        private static StateMachine<OrderStates, OrderTriggers> Configure(StateMachine<OrderStates, OrderTriggers> stateMachine)
+        {
+            // todo configuration states here
+            return stateMachine;
+        }
+    }
+}
